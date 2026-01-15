53cebd73fb9c8082ea276dcb892694759ea15a12
Pete Vagiakos <37168722+Kombatant@users.noreply.github.com>
2026-01-15 00:11:40 -0500

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_krunner_fsearch_FILE /home/kombatant/Dev/scripts/fsearch-krunner/build/bin/kf6/krunner/krunner_fsearch.so)
-set(__QT_DEPLOY_TARGET_krunner_fsearch_TYPE MODULE_LIBRARY)
--- build/CMakeFiles/krunner_fsearch.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for krunner_fsearch.
-# This may be replaced when dependencies are built.
